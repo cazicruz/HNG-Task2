41fe183fd2620c8bb72161311ca8df0d86fefb89
david onwuli <dcazicruz@icloud.com>
2023-09-11 09:31:09 +0100

    added support for queries

--- Router/personRoute.js
+++ Router/personRoute.js
@@ -8,12 +8,13 @@ router.route('/')
   .put(updatePerson)
   .delete(deletePerson)
 
-  
+
 router.route('/:name')
   .get(getPerson)
   .post(createPerson)
-  .put(updatePerson)
   .delete(deletePerson)
+
+router.put('/:name/:newName', updatePerson);
 
 
 module.exports = router;
